5d37d6a9dc07bfb397902f6b7209df727cdf1732
kursatacikgoz <kursatacikgoz@marun.edu.tr>
2020-12-18 20:55:58 +0300

    Add map

--- iletisim.php
+++ iletisim.php
@@ -1,4 +1,4 @@
-﻿<?php include 'header.php' ?>
+﻿﻿<?php include 'header.php' ?>
 
 <div class="container">
 
@@ -30,9 +30,9 @@
 
 			<!-- Google Maps - Go to the bottom of the page to change settings and map location. -->
 			<div id="googlemaps" class="google-map google-map-footer">
-				<iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=<?php echo $ayarcek['ayar_goooglemap']; ?>
-				&q=<?php echo $ayarcek['ayar_adres']; ?>" allowfullscreen>
-				</iframe>
+
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3011.7467549043517!2d29.050624515413446!3d40.987025879302905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cac78eb24e62c3%3A0xf6aeec48ddd5df7c!2sMarmara%20%C3%9Cniversitesi!5e0!3m2!1str!2str!4v1608314081731!5m2!1str!2str" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+				
 
 			</div>
 		</div>
